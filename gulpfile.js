eb629a774b174df9f47a9490253118241044a58e
aaron <aaron@countable.ca>
2018-05-16 00:02:51 -0700

    sass & pug

--- gulpfile.js
+++ gulpfile.js
@@ -8,11 +8,14 @@ const htmlmin = require('gulp-htmlmin');
 const babel = require('gulp-babel');
 const nodemon = require('gulp-nodemon');
 const plumber = require('gulp-plumber');
+const sass = require('gulp-sass');
+const pug = require('gulp-pug');
 
 const paths = {
     scripts: ["src/js/*.js"],
-    styles: ["src/css/*.css"],
-    html: ["src/*.html"]
+    styles: ["src/css/*.css", "src/css/*.scss", "src/css/*.sass"],
+    html: ["src/*.html"],
+    views: ["src/*.pug"]
 }
 
 gulp.task('html', () => {
@@ -23,6 +26,16 @@ gulp.task('html', () => {
         .pipe(sourcemaps.write())
         .pipe(gulp.dest('dist'));
 });
+
+gulp.task('views', () => {
+    return gulp.src(paths.views)
+        .pipe(plumber())
+        .pipe(sourcemaps.init())
+        .pipe(pug())
+        .pipe(sourcemaps.write())
+        .pipe(gulp.dest('dist'));
+});
+
 
 gulp.task('scripts', () => {
     return gulp.src(paths.scripts)
@@ -41,6 +54,7 @@ gulp.task('styles', () => {
     return gulp.src(paths.styles)
         .pipe(plumber())
         .pipe(sourcemaps.init())
+        .pipe(sass().on('error', sass.logError))
         .pipe(concat("styles.css"))
         .pipe(autoprefixer())
         .pipe(cleanCSS())
@@ -53,6 +67,7 @@ gulp.task('watch', function() {
         gulp.watch(paths.scripts, ['scripts']);
         gulp.watch(paths.styles, ['styles']);
         gulp.watch(paths.html, ['html']);
+        gulp.watch(paths.views, ['views']);
         nodemon({
             script: 'app.js',
             ext: 'js',
@@ -61,5 +76,5 @@ gulp.task('watch', function() {
 
 });
 
-gulp.task('default', ['scripts', 'styles', 'html']);
+gulp.task('default', ['scripts', 'styles', 'html', 'views']);
 gulp.task('develop', ['watch']);
